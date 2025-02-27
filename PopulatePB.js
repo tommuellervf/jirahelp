d4bbf55ad750abe72af7cf151da797fa807ca424
tommuellervf <tom.mueller02@vodafone.com>
2025-02-27 08:44:50 +0100

    Update PopulatePB.js

--- PopulatePB.js
+++ PopulatePB.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PopulatePB with Map Alert
 // @namespace    none
-// @version      1.0.3
+// @version      1.0.4
 // @description  Address data from Jira to PB and reload on map alert.
 // @include      https://nd-jira.unity.media.corp/*
 // @include      https://vfde-nig.ker-l-nigmsn01p.unity.media.corp:30443/physical_browser/index.html*
