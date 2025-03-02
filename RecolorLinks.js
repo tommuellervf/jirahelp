bb08f25cd470ce808a66299f431ed3d84fa84517
tommuellervf <tom.mueller02@vodafone.com>
2025-03-02 13:57:02 +0100

    Update RecolorLinks.js

--- RecolorLinks.js
+++ RecolorLinks.js
@@ -1,41 +1,43 @@
 // ==UserScript==
 // @name         Hauptskript zum ändern der Farbe der Links in Jira
 // @namespace    none
-// @version      1.0.1
+// @version      1.0.2
 // @description  Ändert Farbe der Links in Jira
 // @include      https://nd-jira.unity.media.corp/*
-// @grant        GM.xmlHttpRequest
+// @grant        none
 // @updateURL    https://raw.githubusercontent.com/tommuellervf/jirahelp/main/RecolorLinks.js
 // @downloadURL  https://raw.githubusercontent.com/tommuellervf/jirahelp/main/RecolorLinks.js
-// @grant        GM_addStyle
 // @noframes
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    if (typeof GM_addStyle === 'undefined') {
-        alert('Please enable the "GM_addStyle" permission for this script to work.');
-        return;
-    }
+    // Set body class for easier styling
+    document.body.classList.add('jira-link-colorizer');
 
-    document.body.id = 'jira-link-colorizer';
-    const css = `
-  #jira-link-colorizer td.issuelinks a {
-
-  }
-  #jira-link-colorizer td.issuelinks a[href*="EXTDL"] {
-    color: red !important;
-    font-weight: bold !important;
-  }
-  #jira-link-colorizer td.issuelinks a[href*="LINIE"] {
-    color: green !important;
-    font-weight: bold !important;
-  }
-  #jira-link-colorizer td.issuelinks a[href*="SER"] {
-    color: blue !important;
-    font-weight: bold !important;
-  }
+    // Apply CSS styles directly to the DOM
+    const style = document.createElement('style');
+    style.textContent = `
+        .jira-link-colorizer td.issuelinks a {
+            /* Default link styles if needed */
+        }
+        .jira-link-colorizer td.issuelinks a[href*="EXTDL"] {
+            color: red !important;
+            font-weight: bold !important;
+        }
+        .jira-link-colorizer td.issuelinks a[href*="LINIE"] {
+            color: green !important;
+            font-weight: bold !important;
+        }
+        .jira-link-colorizer td.issuelinks a[href*="SER"] {
+            color: blue !important;
+            font-weight: bold !important;
+        }
+        .jira-link-colorizer td.issuelinks a[href*="ANDE"] {
+            color: pink !important;
+            font-weight: bold !important;
+        }
     `;
-    GM_addStyle(css);
+    document.head.appendChild(style);
 })();
