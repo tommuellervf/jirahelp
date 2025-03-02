bcd43ed949c9ec4250635ec27d5b14db3c6932b2
tommuellervf <tom.mueller02@vodafone.com>
2025-03-02 13:01:08 +0100

    Update RecolorDuedate.js

--- RecolorDuedate.js
+++ RecolorDuedate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Recolor Duedate
 // @namespace    none
-// @version      1.0.2
+// @version      1.0.3
 // @description  Recolor Duedate
 // @include      https://nd-jira.unity.media.corp/*
 // @updateURL    https://raw.githubusercontent.com/tommuellervf/jirahelp/main/RecolorDuedate.js
@@ -14,6 +14,17 @@
 (function() {
   'use strict';
 
+  function parseDate(dateString) {
+    const parts = dateString.split('.');
+    if (parts.length === 3) {
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1; // Month is 0-indexed
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+    }
+    return null;
+  }
+
   function styleDueDates() {
     const dueDateCells = Array.from(document.querySelectorAll('body#jira tr.issuerow td.duedate'));
     const today = new Date();
@@ -21,15 +32,23 @@
 
     dueDateCells.forEach(cell => {
       const dateText = cell.textContent.trim();
-      const date = new Date(dateText);
-      const isFuture = date > today;
+      const date = parseDate(dateText);
+
+      if (!date) {
+        console.error('Failed to parse date:', dateText);
+        return;
+      }
+
+      const isPast = date <= today;
 
       cell.style.textAlign = 'center';
 
-      if (isFuture) {
-        cell.style.backgroundColor = 'nothing';
-      } else {
+      if (isPast) {
         cell.style.backgroundColor = '#ffa0a0';
+      }
+      else
+      {
+        cell.style.backgroundColor = '#f8fff8';
       }
     });
   }
@@ -45,4 +64,5 @@
   });
 
   observer.observe(document.body, { childList: true, subtree: true });
+
 })();
