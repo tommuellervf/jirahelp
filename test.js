22c9c3f7a20ab0d7141d249004c790b0de2ddeac
tommuellervf <tom.mueller02@vodafone.com>
2025-03-16 22:18:02 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hauptskript für Kontextmenü Test
 // @namespace    none
-// @version      1.0.27
+// @version      1.0.28
 // @description  Erstellt das Kontextmenü basierend auf externer Menüstruktur
 // @include      *
 // @grant        GM.xmlHttpRequest
